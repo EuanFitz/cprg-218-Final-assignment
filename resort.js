7db114729ad44f01ad11eb4092b9c649fc61cf44
EuanFitz <euan.fitzpatty@gmail.com>
2025-03-30 21:22:26 -0600

    Trying to finalize the header

--- resort.js
+++ resort.js
@@ -1,12 +1,7 @@
 const navMenu = document.getElementById('nav-menu');
 const headerPic = document.getElementById('header-pic');
-const menuHeight = document.getElementById('menu-height');
 
 hamburger.addEventListener('click', function(){
     navMenu.classList.toggle('active');
     hamburger.classList.toggle('active');
 });
-/*const headerHeight = (headerPic.clientHeight - 120);
-console.log(headerHeight);
-
-menuHeight.style.paddingTop = headerHeight+'px';*/
