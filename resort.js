b55959712fcb39e14b3ca93623eb0c09520ecbdd
EuanFitz <euan.fitzpatty@gmail.com>
2025-03-31 18:15:05 -0600

    Adding Weather API

--- resort.js
+++ resort.js
@@ -5,3 +5,20 @@ hamburger.addEventListener('click', function(){
     navMenu.classList.toggle('active');
     hamburger.classList.toggle('active');
 });
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=hawaii&appid=76e7024c8cdf08475411f848e7840011&units=metric')
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    
+    temperature.textContent = "Temperature: "+ Math.floor(data.main.temp)+ "\u2103";
+   
+    //Icon
+    const weatherIcon = "https://openweathermap.org/img/wn/"+ data.weather[0].icon+"@2x.png"
+    console.log(icon);
+
+    icon.src = weatherIcon;
+    icon.alt = data.weather[0].description;
+    
+});
+
