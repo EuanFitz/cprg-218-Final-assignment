445cc47f690db262d7b2f77afe76c80e49ecb0bd
EuanFitz <euan.fitzpatty@gmail.com>
2025-04-01 14:42:45 -0600

    setting up carousel for activities
    The java script is giving me issues but I have the main layout ready to go

--- resort.js
+++ resort.js
@@ -22,3 +22,39 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=hawaii&appid=76e7024c8c
     
 });
 
+/*Activity carousel*/
+activitynext.addEventListener('click', function(){
+    
+    if(activityimg.src = 'media/1.jpg'){
+        activityimg.src = 'media/2.jpg'
+        activitytype.textContent = 'Infinity Pool'
+        activitydesc.textContent = 'Relax by our stunning oceanfront infinity pool.'
+    }
+
+    if(activityimg.src = 'media/2.jpg'){
+        activityimg.src = 'media/3.jpg'
+        activitytype.textContent = 'World-Class Spa'
+        activitydesc.textContent = 'Rejuvenate your mind and body at our spa.'
+    }
+
+     if(activityimg.src = 'media/3.jpg'){
+        activityimg.src = 'media/4.jpg'
+        activitytype.textContent = 'Exquisite Dining'
+        activitydesc.textContent = 'Savor culinary delights at our restaurants.'
+    }
+
+
+    if(activityimg.src = 'media/4.jpg'){
+        activityimg.src = 'media/1.jpg'
+        activitytype.textContent = 'Private Beach Access'
+        activitydesc.textContent = 'Step onto our pristine, white-sand beach.'
+    }
+});
+
+activityback.addEventListener('click', function(){
+    if(activityimg.src = 'media/2.jpg'){
+        activityimg.src = 'media/1.jpg'
+        activitytype.textContent = 'Private Beach'
+        activitydesc.textContent = 'Step onto our pristine, white-sand beach.'
+    }
+});
